939b3fcd4eace734ad9eb43c70be8cae6dc482fe
Ateş <ates@betandworks.com>
2024-07-19 13:27:28 +0300

    time rang

--- src/app/components/shared/TimeTabs.jsx
+++ src/app/components/shared/TimeTabs.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useCallback, useEffect, Fragment } from 'react';
+import React, { memo, useState, useCallback, useEffect, Fragment, useMemo } from 'react';
 import dayjs from 'dayjs';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Typography } from '@mui/material';
 import { t } from 'i18next';
@@ -6,12 +6,58 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-
+import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
 
 function TimeTabs({topElement, children, onChange, justify = "end" || "between" || "start"}) {
   const [timeRange, setTimeRange] = useState(0);
   const [time, setTime] = useState([dayjs(), dayjs().add(7,"days")]);
   const [open, setOpen] = useState(false);
+  const shortcutsItems = useMemo(() => [
+    {
+      label: t("Dashboard.dateRange.yesterday"),
+      getValue: () => {
+        const today = dayjs().subtract(1,"day");
+        return [today.startOf('day'), today.endOf('day')];
+      },
+    },
+    {
+      label: t("Dashboard.dateRange.today"),
+      getValue: () => {
+        const today = dayjs();
+        return [today.startOf('day'), today.endOf('day')];
+      },
+    },
+    {
+      label: t("Dashboard.dateRange.thisWeek"),
+      getValue: () => {
+        const today = dayjs();
+        return [today.startOf('week'), today.endOf('week')];
+      },
+    },
+    {
+      label: t("Dashboard.dateRange.last7Day"),
+      getValue: () => {
+        const today = dayjs();
+        return [today.subtract(7, 'day'), today];
+      },
+    },
+    {
+      label: t("Dashboard.dateRange.thisMonth"),
+      getValue: () => {
+        const today = dayjs();
+        return [today.startOf('month'), today.endOf('month')];
+      },
+    },
+    {
+      label: t("Dashboard.dateRange.lastMonth"),
+      getValue: () => {
+        const today = dayjs().startOf("month").subtract(1, "days");
+        const startOfNextMonth = today.startOf("month");
+        return [startOfNextMonth, startOfNextMonth.endOf('month')];
+      },
+    },
+    // { label: 'Reset', getValue: () => [null, null] },
+  ], []);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -46,64 +92,79 @@ function TimeTabs({topElement, children, onChange, justify = "end" || "between" 
       <Grid container spacing={2} alignItems="center">
         {topElement && topElement}
         {justify==="end" && <Grid sx={{ flex: 1 }} />}
-        <Grid item>
-          <Button variant={timeRange===0 ? "contained" : "outlined"} onClick={() => changeRange(0)} color="primary">Bugün</Button>
-        </Grid>
-        <Grid item>
-          <Button variant={timeRange===1 ? "contained" : "outlined"} onClick={() => changeRange(1)}>Dün</Button>
-        </Grid>
-        <Grid item>
-          <Button variant={timeRange===2 ? "contained" : "outlined"} onClick={() => changeRange(2)}>Son 7 Gün</Button>
-        </Grid>
-        <Grid item>
-          <Button variant={timeRange===3 ? "contained" : "outlined"} onClick={() => changeRange(3)}>Son 15 Gün</Button>
-        </Grid>
-        <Grid item>
-          <Button variant={timeRange===4 ? "contained" : "outlined"} onClick={() => changeRange(4)}>Son 30 Gün</Button>
-        </Grid>
-        <Grid item>
-          <Button variant={timeRange===5 ? "contained" : "outlined"} onClick={() => changeRange(5)}>Bu Ay</Button>
-        </Grid>
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===0 ? "contained" : "outlined"} onClick={() => changeRange(0)} color="primary">Bugün</Button>*/}
+        {/*</Grid>*/}
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===1 ? "contained" : "outlined"} onClick={() => changeRange(1)}>Dün</Button>*/}
+        {/*</Grid>*/}
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===2 ? "contained" : "outlined"} onClick={() => changeRange(2)}>Son 7 Gün</Button>*/}
+        {/*</Grid>*/}
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===3 ? "contained" : "outlined"} onClick={() => changeRange(3)}>Son 15 Gün</Button>*/}
+        {/*</Grid>*/}
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===4 ? "contained" : "outlined"} onClick={() => changeRange(4)}>Son 30 Gün</Button>*/}
+        {/*</Grid>*/}
+        {/*<Grid item>*/}
+        {/*  <Button variant={timeRange===5 ? "contained" : "outlined"} onClick={() => changeRange(5)}>Bu Ay</Button>*/}
+        {/*</Grid>*/}
         {justify==="between" && <Grid sx={{ flex: 1 }} />}
         <Grid item>
-          <Button variant="outlined" onClick={handleClickOpen}>
-            {time?.[0] && time?.[1] ? `${dayjs(time?.[0]).format("DD.MM.YYYY")} - ${dayjs(time?.[1]).format("DD.MM.YYYY")}` : 'Tarih Seç'}
-          </Button>
+          {/*<Button variant="outlined" onClick={handleClickOpen}>*/}
+          {/*  {time?.[0] && time?.[1] ? `${dayjs(time?.[0]).format("DD.MM.YYYY")} - ${dayjs(time?.[1]).format("DD.MM.YYYY")}` : 'Tarih Seç'}*/}
+          {/*</Button>*/}
+
+
+
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"tr"}>
+            <DemoContainer components={[
+              'DateRangePicker',
+            ]}>
+              <DemoItem
+                label={
+                  <></>
+                }
+                component="DateRangePicker"
+              >
+                <DateRangePicker
+                  defaultValue={time}
+                  localeText={{
+                    start: '',
+                    end: '',
+                  }}
+                  format="DD/MM/YYYY"
+                  onChange={(value) => setTime(value)}
+                  slots={{field: SingleInputDateRangeField}}
+                  slotProps={{
+                    shortcuts: {
+                      items: shortcutsItems,
+                    },
+                    actionBar: {actions: []}
+                  }}
+                  format={"DD/MM/YYYY"}
+                  calendars={2}
+                />
+              </DemoItem>
+            </DemoContainer>
+
+          </LocalizationProvider>
+
+
         </Grid>
         {justify==="start" && <Grid sx={{ flex: 1 }} />}
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>{t("Dashboard.Financial Graph.Select Date Range")}</DialogTitle>
-          <DialogContent>
-            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"tr"}>
-              <DemoContainer components={[
-                'DateRangePicker',
-              ]}>
-                <DemoItem
-                  label={
-                    <></>
-                  }
-                  component="DateRangePicker"
-                >
-                  <DateRangePicker
-                    defaultValue={time}
-                    localeText={{
-                      start: '',
-                      end: '',
-                    }}
-                    format="DD/MM/YYYY"
-                    onChange={(value) => setTime(value)}
-                  />
-                </DemoItem>
-              </DemoContainer>
+        {/*<Dialog open={open} onClose={handleClose}>*/}
+        {/*  <DialogTitle>{t("Dashboard.Financial Graph.Select Date Range")}</DialogTitle>*/}
+        {/*  <DialogContent>*/}
+        {/*    */}
 
-            </LocalizationProvider>
-
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">İptal</Button>
-            <Button onClick={handleClose} color="primary">Tamam</Button>
-          </DialogActions>
-        </Dialog>
+        {/*  </DialogContent>*/}
+        {/*  <DialogActions>*/}
+        {/*    <Button onClick={handleClose} color="primary">İptal</Button>*/}
+        {/*    <Button onClick={handleClose} color="primary">Tamam</Button>*/}
+        {/*  </DialogActions>*/}
+        {/*</Dialog>*/}
       </Grid>
       {children(time)}
     </Fragment>
