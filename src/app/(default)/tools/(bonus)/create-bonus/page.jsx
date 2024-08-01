8c8e2398474ac7cff35381a127513ec9c796ff31
Aras Batur <arasbatur@betandworks.com>
2024-08-01 22:59:46 +1000

    text editor fix

--- src/app/(default)/tools/(bonus)/create-bonus/page.jsx
+++ src/app/(default)/tools/(bonus)/create-bonus/page.jsx
@@ -37,7 +37,7 @@ const multipleSelectItems = [
 ];
 
 function Page() {
-  const [descriptionTextState, setDescriptionTextState] = useState();
+  const [descriptionTextState, setDescriptionTextState] = useState(`<b>Lorem Ipsum</b>`);
   const [startDateTime, setStartDateTime] = React.useState(dayjs());
   const [finishDateTime, setFinishDateTime] = React.useState(dayjs());
 
@@ -418,14 +418,9 @@ function Page() {
                       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
                       toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
                       tinycomments_mode: 'embedded',
-                      tinycomments_author: 'Author name',
-                      mergetags_list: [
-                        { value: 'First.Name', title: 'First Name' },
-                        { value: 'Email', title: 'Email' },
-                      ],
                       ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
                     }}
-                    initialValue="Welcome to TinyMCE!"
+                    initialValue={descriptionTextState}
                   />
                 </Grid>
 
