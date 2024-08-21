a88c174e9fe476bd4bf0643a0539a543d883cae2
Ateş <ates@betandworks.com>
2024-08-21 10:26:57 +0300

    detail

--- src/app/(default)/accounting-management/(deposit-transactions)/(new-requests)/page.jsx
+++ src/app/(default)/accounting-management/(deposit-transactions)/(new-requests)/page.jsx
@@ -14,6 +14,121 @@ import ParentCard from '@/app/components/shared/ParentCard';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Tooltip from '@mui/material/Tooltip';
+import ChildCard from '@/app/components/shared/ChildCard';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+
+const DetailCard = memo(function DetailCardComp({id, row}){
+
+  const [data, setData] = useState({
+    page: 1,
+    pageSize: 10,
+    totalData: 20,
+    totalPage: 1,
+    columns: [],
+    rows: [],
+  });
+  useEffect(() => {
+    const columns = [
+      {
+        field: 'category',
+        headerName: t('pages.accounting-management.category'),
+        flex:1,
+      },
+      {
+        field: 'provider',
+        headerName: t('pages.accounting-management.provider'),
+        flex:1,
+        // width: 200
+      },
+      {
+        field: 'amount',
+        headerName: t('pages.accounting-management.amount'),
+        // width: 200
+      },
+      {
+        field: 'confirmedAt',
+        headerName: t('pages.accounting-management.confirmedAt'),
+        flex:1,
+        // width: 200
+      }
+    ];
+
+    const rows = Array.from(Array(5)).map(() => ({
+      id: uniqueId(),
+      category: faker.helpers.arrayElement(['Papara', 'Payfix', "CepBank"]),
+      provider: faker.helpers.arrayElement(['Papara Key', 'Garanti', "Finansbank"]),
+      confirmedAt: faker.date.recent().toLocaleString('tr-TR', { dateStyle: 'short', timeStyle: 'short' }),
+      amount: faker.commerce.price(1000, 100000, 2) + '₺'
+    }));
+
+    setData((prev) => ({
+      ...prev,
+      columns: columns,
+      rows: rows,
+      pageSize: rows?.length,
+      totalPage: Math.floor(data?.pageSize / (rows?.length || 1)),
+    }));
+  }, []);
+
+  return(
+    <ChildCard>
+      <Box className={"flex md:flex-row flex-col gap-2"}>
+        <Box className={"md:flex-1"}>
+          <Typography variant="h4">{t("pages.accounting-management.transactionDetails")}</Typography>
+
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.amount")}</TableCell>
+                <TableCell>{row?.amount}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.bank")}</TableCell>
+                <TableCell>{row?.bank}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.iban")}</TableCell>
+                <TableCell>{row?.iban}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.accountCode")}</TableCell>
+                <TableCell>{row?.accountCode}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.accountNo")}</TableCell>
+                <TableCell>{row?.accountNo}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.idNumber")}</TableCell>
+                <TableCell>{row?.idNumber}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component={"th"}>{t("pages.accounting-management.userNote")}</TableCell>
+                <TableCell>{row?.note}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+        </Box>
+        <Box className={"md:flex-1"}>
+          <Typography variant="h4">{t("pages.accounting-management.userLastConfirmedTransactions")}</Typography>
+
+          <DataTable
+            search={false}
+            data={data}
+            toolbar={false}
+          />
+
+        </Box>
+      </Box>
+    </ChildCard>
+  )
+});
 
 function Page() {
   const title = t("pages.accounting-management.newRequests");
@@ -84,11 +199,11 @@ function Page() {
                 <IconInfoCircle className={"text-amber-500"} />
               </Tooltip>
             ] : []),
-            <IconButton onClick={() => {
-
-            }}>
-              <IconChevronRight />
-            </IconButton>,
+            // <IconButton onClick={() => {
+            //
+            // }}>
+            //   <IconChevronRight />
+            // </IconButton>,
             // <IconButton onClick={() => router.push(`/users/${e?.row?.username}`)}>
             //   <IconPencil />
             // </IconButton>
@@ -106,7 +221,12 @@ function Page() {
       provider: faker.helpers.arrayElement(['Papara Key', 'Garanti', "Finansbank"]),
       requestTime: faker.date.recent().toLocaleString('tr-TR', { dateStyle: 'short', timeStyle: 'short' }),
       amount: faker.commerce.price(1000, 100000, 2) + '₺',
-      note: "Some note "+uniqueId()
+      note: "Some note "+uniqueId(),
+      bank: faker.helpers.arrayElement(['AKBANK', 'FİNANSBANK', "GARANTİ"]),
+      iban: "TR"+faker.datatype.number({ min: 1000000, max: 9999999 }).toString(),
+      accountCode: faker.datatype.number({ min: 100, max: 999 }).toString(),
+      accountNo: faker.datatype.number({ min: 10000, max: 99999 }).toString(),
+      idNumber: faker.datatype.number({ min: 1000000, max: 9999999 }).toString()
   }));
 
     setData((prev) => ({
@@ -156,6 +276,8 @@ function Page() {
           search={false}
           data={data}
           toolbar={false}
+          getDetailPanelContent={({ id,row }) => <DetailCard id={id} row={row} />}
+          getDetailPanelHeight={({ row }) => 'auto'} // Height based on the content.
         />
       </ParentCard>
     </Box>
