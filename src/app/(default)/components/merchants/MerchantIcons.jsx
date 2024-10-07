6869aece3a8c6f469c8e83e7d72ca3b516217b1e
Aras Batur <arasbatur@betandworks.com>
2024-10-07 19:55:24 +1100

    Jerand icons updated

--- src/app/(default)/components/merchants/MerchantIcons.jsx
+++ src/app/(default)/components/merchants/MerchantIcons.jsx
@@ -1,5 +1,5 @@
 import React, {Fragment, memo} from 'react';
-import {Fab,  Stack} from "@mui/material";
+import { Stack } from "@mui/material";
 import {
   IconBuildingStore,
   IconCheck,
@@ -15,30 +15,30 @@ function MerchantIcons({children}) {
   return (
     <Fragment>
       <Stack direction="row" spacing={1}>
-        <Fab size="small" sx={{ backgroundColor: '#ff7300' }} >
-          <IconPlayerPause size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#1fcd36' }}>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-orange-500"}>
+          <IconPlayerPause size={16} color="white" />
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-green-500"}>
           <IconCircle size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#121212' }} disabled>
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-black"}>
           <IconLock size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#1fcd36' }} >
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-green-500"}>
           <IconCheck size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#ffd600' }} >
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-yellow-400"}>
           <IconStar size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#121212' }} >
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-black"}>
           <IconBuildingStore size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#CD201F' }} >
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-red-600"}>
           <IconHeart size={16} color="white"/>
-        </Fab>
-        <Fab size="small" sx={{ backgroundColor: '#1877F2' }} disabled>
+        </div>
+        <div className={"flex h-7 w-7 items-center justify-center rounded-full bg-blue-600"}>
           <IconUserCheck size={16} color="white"/>
-        </Fab>
+        </div>
       </Stack>
     </Fragment>
   );
